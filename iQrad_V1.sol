9acc87d52c8abc41db0d8336e812229a13382ecf
iChain <143358269+iChainBlock@users.noreply.github.com>
2024-02-17 19:59:15 +0200

    Update iQrad_V1.sol

--- iQrad_V1.sol
+++ iQrad_V1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,20 +10,20 @@ interface iGoldContract {
     function sell(uint256 _iGoldAmount) external;
 }
 
-contract iQrad_V1_test is Ownable {
+contract iQrad_V1 is Ownable {
 
     uint256 public constant oneMonth = 30 days;
 
     iGoldContract public iGoldc =
-        iGoldContract(0xf8e81D47203A594245E36C48e151709F0C19fBe8);//(0x56511242C7FED63DC2d7B83B2e124D6ba925b125);
+        iGoldContract(0x9440146ea1dF0142eE1892602416DA896c7876E8);
 
     address public deadWallet = 0x000000000000000000000000000000000000dEaD;
 
-    address public islamiToken = 0xd9145CCE52D386f254917e481eB44e9943F39138;//0x942714c1da04Cc66362aad2132A36e896491d353;
-    address public iGoldToken = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;//0x56511242C7FED63DC2d7B83B2e124D6ba925b125;
-    address public usdtToken = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;//0xC7185282aafDD110E549f2A06167CB81f3F3E1d0;
+    address public islamiToken = 0x9c891326Fd8b1a713974f73bb604677E1E63396D;
+    address public iGoldToken = 0x9440146ea1dF0142eE1892602416DA896c7876E8;
+    address public usdtToken = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
 
-    address public feeReceiver = address(this); //0xa958C8294d7EbC2d46172c294Ac2841875827Da8;
+    address public feeReceiver = address(this);//0x1be6cF82aC405cC46D35895262Fa83f582D42884;
 
     uint256 public oneTimeFee = 10000 * (1e7); // 10,000 ISLAMI
 
@@ -31,8 +31,6 @@ contract iQrad_V1_test is Ownable {
     uint256 public minLoanAmountOneYear = 500 * (1e6);
     uint256 public minLoanAmountDefault = 100 * (1e6);
     uint256 public serviceFee = 3000 * (1e7); // 2 USDT in ISLAMI
-
-    uint256 public iGoldPrice = uint256(iGoldc.getIGoldPrice());
 
     uint256 public usdtVault;
     uint256 public usdtInLoans;
@@ -61,11 +59,6 @@ contract iQrad_V1_test is Ownable {
         uint256 duration;
     }
 
-    struct InvestorVaults {
-        address investor;
-        AngelInvestor[] vaults;
-    }
-
     struct User {
         bool hasFile;
         uint256 collateral;
@@ -80,11 +73,9 @@ contract iQrad_V1_test is Ownable {
     }
 
     mapping(address => mapping(uint256 => AngelInvestor)) public angelInvestors;
-    mapping(address => uint256) public nextVaultId;
-    mapping(address => uint256) public vaultsCount;
+    mapping(address => uint256) private nextVaultId;
     mapping(address => User) public users;
     mapping(address => bool) public isInvestor;
-    mapping(address => bool) public hasLoan;
 
     event AngelInvestorDeposited(
         address indexed investor,
@@ -115,18 +106,8 @@ contract iQrad_V1_test is Ownable {
         _;
     }
 
-    constructor() Ownable(msg.sender){
+    constructor() {
         IERC20(islamiToken).approve(iGoldToken, type(uint256).max);
-    }
-
-    function setISLAMIaddress(address _new) external onlyOwner{
-        require(_new != address(0),"Zero address");
-        islamiToken = _new;
-    }
-
-    function setiGoldAddress(address _new) external onlyOwner{
-        require(_new != address(0),"Zero address");
-        iGoldToken = _new;
     }
 
     function approveISLAMI() external {
@@ -142,7 +123,7 @@ contract iQrad_V1_test is Ownable {
         external
     {
         require(amount > 0, "Deposit amount must be greater than 0");
-        require(_duration >= 1, "Deposite should be at least for 9 Months"); //9
+        require(_duration >= 9, "Deposite should be at least for 9 Months");
         if(nextVaultId[msg.sender] == 0){
             nextVaultId[msg.sender] = 1;
         }
@@ -159,11 +140,10 @@ contract iQrad_V1_test is Ownable {
         investor.availableAmount += amount;
         investor.vault = vaultId; // Set the vault ID
         investor.depositTime = block.timestamp; // Set the deposit time
-        investor.duration = (_duration * 300 ) + block.timestamp;
+        investor.duration = (_duration * 30 days) + block.timestamp;
 
         usdtVault += amount;
         nextVaultId[msg.sender]++; // Increment the vault ID for the next deposit
-        vaultsCount[msg.sender]++;
 
         if (!isInvestor[msg.sender]) {
             isInvestor[msg.sender] = true;
@@ -179,39 +159,26 @@ contract iQrad_V1_test is Ownable {
         returns (AngelInvestor[] memory)
     {
         uint256 vaultCount = nextVaultId[investor];
+        AngelInvestor[] memory investorVaults = new AngelInvestor[](vaultCount);
 
-        // First pass to count non-empty vaults
-        uint256 nonEmptyCount = 0;
-        for (uint256 i = 1; i < vaultCount; i++) {
-            if (angelInvestors[investor][i].depositAmount > 0) {
-                nonEmptyCount++;
-            }
-        }
-
-        // Initialize the array with the size of non-empty vaults
-        AngelInvestor[] memory investorVaults = new AngelInvestor[](nonEmptyCount);
-
-        // Second pass to fill the array
-        uint256 arrayIndex = 0;
-        for (uint256 i = 1; i < vaultCount; i++) {
-            if (angelInvestors[investor][i].depositAmount > 0) {
-                investorVaults[arrayIndex] = angelInvestors[investor][i];
-                arrayIndex++;
-            }
+        for (uint256 i = 0; i < vaultCount; i++) {
+            AngelInvestor storage vault = angelInvestors[investor][i];
+            investorVaults[i] = vault;
         }
 
         return investorVaults;
     }
 
-
-    function withdrawFromAngelInvestor(uint256 vaultId)
+    function withdrawFromAngelInvestor(uint256 vaultId, uint256 amount)
         external
     {
         require(vaultId < nextVaultId[msg.sender], "Invalid vault ID");
         AngelInvestor storage investor = angelInvestors[msg.sender][vaultId];
+        require(
+            investor.availableAmount >= amount,
+            "Insufficient deposit amount"
+        );
         require(block.timestamp >= investor.duration, "Withdraw is not yet");
-
-        uint256 amount = investor.availableAmount;
 
         require(
             IERC20(usdtToken).transfer(msg.sender, amount),
@@ -224,11 +191,7 @@ contract iQrad_V1_test is Ownable {
 
         if (investor.depositAmount == 0) {
             investor.depositTime = 0;
-            investor.vault = 0;
-            vaultsCount[msg.sender]--;
-            if(vaultsCount[msg.sender] == 0){
-                removeInvestorIfNoVaults(msg.sender);
-            }
+            investor.vault = 0; // Consider resetting the vault ID if needed
         }
 
         emit AngelInvestorWithdrawn(msg.sender, vaultId, amount);
@@ -266,7 +229,7 @@ contract iQrad_V1_test is Ownable {
     {
         require(
             IERC20(iGoldToken).transferFrom(user, address(this), amount),
-            "iQrad: iGold Transfer failed"
+            "Transfer failed"
         );
         iGoldVault += amount;
         users[user].collateral += amount;
@@ -278,12 +241,11 @@ contract iQrad_V1_test is Ownable {
         uint256 collateralAmount,
         LoanTenure tenure
     ) internal hasFile(user) {
-        uint256 _iGoldPrice = getiGoldPrice();
-
-        require(!hasLoan[user],"iQrad: User has loan already");
+        uint256 iGoldPrice = uint256(iGoldContract(iGoldToken).getIGoldPrice());
+        require(iGoldPrice > 0, "Invalid iGold price");
 
         // Calculate the total value of the iGold collateral
-        uint256 collateralValue = (collateralAmount * _iGoldPrice) / (1e8);
+        uint256 collateralValue = collateralAmount * iGoldPrice;
 
         // Calculate the maximum loan amount as 70% of the collateral value
         uint256 amount = ((collateralValue * 70) / 100) / (1e2); //(1e2) to handel the decimal difference
@@ -295,13 +257,13 @@ contract iQrad_V1_test is Ownable {
             minLoanAmount = minLoanAmountSixMonths;
         }
 
-        require(amount >= minLoanAmount, "iQrad: Loan amount is less than minimum required for the duration");
+        require(amount >= minLoanAmount, "Loan amount is less than minimum required for the duration");
 
         // uint256 serviceFee = (amount * serviceFeePercentage) / 100;
         // Ensure user has sufficient balance, calculate loan logic, transfer service fee, etc.
 
         // Transfer service fee to fee receiver
-        IERC20(islamiToken).transferFrom(msg.sender, deadWallet, serviceFee);
+        IERC20(islamiToken).transfer(deadWallet, serviceFee);
 
         require(
             IERC20(usdtToken).balanceOf(address(this)) >= amount,
@@ -336,16 +298,14 @@ contract iQrad_V1_test is Ownable {
         users[user].loanAmount = amount;
         users[user].monthlyFee = monthlyFee;
         users[user].lastPaymentTime = block.timestamp;
-        users[user].nextPaymentTime = block.timestamp + 300; // 30 days
+        users[user].nextPaymentTime = block.timestamp + oneMonth;
         users[user].vaultUsed = selectedVaultId;
         users[user].angel = _angel;
         users[user].status = LoanStatus.ACTIVE;
         users[user].tenure = tenure;
 
-        hasLoan[user] = true;
-
         emit LoanTaken(user, amount);
-        require(IERC20(usdtToken).transfer(user, amount), "iQrad: USDT transfer error");
+        IERC20(usdtToken).transfer(user, amount);
     }
 
     function _selectVaultForLoan(uint256 loanAmount, LoanTenure tenure)
@@ -353,68 +313,39 @@ contract iQrad_V1_test is Ownable {
         view
         returns (address, uint256)
     {
-        require(loanAmount > 0, "Loan amount must be greater than zero"); // Ensure loan amount is positive
-
-        // Validate the loan tenure
         uint256 tenureDuration = _getTenureDuration(tenure);
-        require(tenureDuration > 0, "Invalid loan tenure"); // Ensure tenure duration is valid
-
         uint256 earliestEndTime = block.timestamp + tenureDuration;
         uint256 selectedVaultId = 0;
-        address selectedInvestorAddress;
+        address investorAddress;
         uint256 selectedVaultEndTime = type(uint256).max;
-
-        bool vaultFound = false; // Flag to check if a suitable vault is found
 
         // Iterate over all investors
         for (uint256 i = 0; i < investors.length; i++) {
-            address investorAddress = investors[i];
-            uint256 vaultCount = nextVaultId[investorAddress] - 1;
-
-            // Ensure the investor has at least one vault
-            require(vaultCount > 0, "Investor has no vaults");
+            investorAddress = investors[i];
+            uint256 vaultCount = nextVaultId[investorAddress];
 
             // Iterate over all vaults for each investor
             for (uint256 vaultId = 1; vaultId <= vaultCount; vaultId++) {
-                AngelInvestor storage vault = angelInvestors[investorAddress][vaultId];
+                AngelInvestor storage vault = angelInvestors[investorAddress][
+                    vaultId
+                ];
 
                 // Check if the vault has enough USDT and the duration is suitable
-                if (vault.availableAmount >= loanAmount && vault.duration >= earliestEndTime) {
+                if (
+                    vault.availableAmount >= loanAmount &&
+                    vault.duration >= earliestEndTime
+                ) {
                     // Select the vault with the earliest end time that meets the criteria
                     if (vault.duration < selectedVaultEndTime) {
                         selectedVaultId = vaultId;
                         selectedVaultEndTime = vault.duration;
-                        selectedInvestorAddress = investorAddress;
-                        vaultFound = true;
                     }
                 }
             }
         }
 
-        require(vaultFound, "No suitable vault found"); // Ensure a suitable vault is found
-
-        return (selectedInvestorAddress, selectedVaultId);
+        return (investorAddress, selectedVaultId);
     }
-
-
-    function getUSDTAmountForLoan(uint256 _collateralAmount) public view returns (uint256 loanAmount){
-    
-        // Calculate the total value of the iGold collateral
-        uint256 collateralValue = (_collateralAmount * iGoldPrice) / (1e8);
-
-        // Calculate the maximum loan amount as 70% of the collateral value
-        uint256 amount = ((collateralValue * 70) / 100) / (1e2);
-
-        return(amount);
-    }
-
-    function getiGoldPrice() internal returns(uint256){
-        uint256 _iGoldPrice = uint256(iGoldc.getIGoldPrice());
-        require(_iGoldPrice > 0, "Invalid iGold price");
-        iGoldPrice = _iGoldPrice;
-        return(_iGoldPrice);
-    }
-    
 
     function _getTenureDuration(LoanTenure tenure)
         internal
@@ -422,11 +353,11 @@ contract iQrad_V1_test is Ownable {
         returns (uint256)
     {
         if (tenure == LoanTenure.SIX_MONTHS) {
-            return 300 ;
+            return 180 days;
         } else if (tenure == LoanTenure.ONE_YEAR) {
-            return 1000 ;
+            return 365 days;
         } else if (tenure == LoanTenure.TWO_YEARS) {
-            return 3000 ;
+            return 730 days;
         } else {
             revert("Invalid loan tenure");
         }
@@ -451,7 +382,7 @@ contract iQrad_V1_test is Ownable {
         usdtInLoans -= user.monthlyFee;
         usdtVault += user.monthlyFee;
         user.lastPaymentTime = block.timestamp;
-        user.nextPaymentTime = block.timestamp + 300; // 30 days
+        user.nextPaymentTime = block.timestamp + oneMonth;
         emit MonthlyPaymentMade(msg.sender, users[msg.sender].monthlyFee);
 
         AngelInvestor storage investor = angelInvestors[user.angel][
@@ -475,12 +406,11 @@ contract iQrad_V1_test is Ownable {
         );
 
         // Get the current price of iGold in terms of USDT
-        // int256 _iGoldPrice = iGoldc.getIGoldPrice() / (1e2);
-        uint256 _iGoldPrice = getiGoldPrice() / (1e2);
-        require(_iGoldPrice > 0, "Invalid iGold price");
+        int256 iGoldPrice = iGoldc.getIGoldPrice() / (1e2);
+        require(iGoldPrice > 0, "Invalid iGold price");
 
         // Calculate the amount of iGold needed to cover the unpaid monthly fee
-        uint256 iGoldNeeded = (user.monthlyFee * (1e8)) / _iGoldPrice;
+        uint256 iGoldNeeded = (user.monthlyFee * (1e8)) / uint256(iGoldPrice);
 
         // Ensure the user has enough iGold collateral to cover the unpaid monthly fee
         require(
@@ -500,13 +430,12 @@ contract iQrad_V1_test is Ownable {
             user.vaultUsed
         ];
         investor.availableAmount += user.monthlyFee;
-        usdtInLoans -= user.monthlyFee;
 
         // Update the user's iGold collateral and last payment time
         user.collateral -= iGoldNeeded;
         user.loanAmount -= user.monthlyFee;
         user.lastPaymentTime = block.timestamp;
-        user.nextPaymentTime = block.timestamp + 300; // 30 days oneMonth
+        user.nextPaymentTime = block.timestamp + oneMonth;
 
         // Check if this is the last payment
         if (user.loanAmount == 0) {
@@ -549,8 +478,6 @@ contract iQrad_V1_test is Ownable {
         user.angel = address(0);
         user.tenure = LoanTenure.NONE;
 
-        hasLoan[_user] = false;
-
         emit LoanClosed(_user);
     }
 
@@ -584,7 +511,6 @@ contract iQrad_V1_test is Ownable {
         );
 
         iGoldVault -= collateral;
-        usdtInLoans -= remainingLoan;
         user.collateral = 0;
         user.monthlyFee = 0;
         user.lastPaymentTime = 0;
@@ -592,8 +518,6 @@ contract iQrad_V1_test is Ownable {
         user.vaultUsed = 0;
         user.angel = address(0);
         user.tenure = LoanTenure.NONE;
-
-        hasLoan[msg.sender] = false;
 
         emit LoanRepaid(msg.sender);
     }
@@ -645,44 +569,5 @@ contract iQrad_V1_test is Ownable {
         minLoanAmountSixMonths = _sixMonths * (1e6);
         minLoanAmountOneYear = _oneYear * (1e6);
         minLoanAmountDefault = _default * (1e6);
-    }
-
-    function getAllInvestorsVaults()
-        external
-        view
-        returns (InvestorVaults[] memory)
-    {
-        InvestorVaults[] memory allInvestorVaults = new InvestorVaults[](investors.length);
-
-        for (uint256 i = 0; i < investors.length; i++) {
-            address investor = investors[i];
-            uint256 vaultCount = nextVaultId[investor];
-            AngelInvestor[] memory vaults = new AngelInvestor[](vaultCount);
-
-            for (uint256 j = 0; j < vaultCount; j++) {
-                vaults[j] = angelInvestors[investor][j];
-            }
-
-            allInvestorVaults[i] = InvestorVaults({
-                investor: investor,
-                vaults: vaults
-            });
-        }
-
-        return allInvestorVaults;
-    }
-
-    function removeInvestorIfNoVaults(address investor) internal {
-        if (vaultsCount[investor] == 0) { // Assuming 0 or 1 indicates no active vaults
-            // Find the investor in the investors array
-            for (uint256 i = 0; i < investors.length; i++) {
-                if (investors[i] == investor) {
-                    // Swap with the last element and remove the last element
-                    investors[i] = investors[investors.length - 1];
-                    investors.pop();
-                    break;
-                }
-            }
-        }
     }
 }
